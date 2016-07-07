c207c604f35c83b491b9798c9190f4f0a2258766
ts33 <timothysoh33@gmail.com>
2016-07-08 00:07:39 +0800

    Created a basic Hello World app

---
+++ app/src/test/java/com/ts33/timejournal/CalculationUnitTest.java
@@ -0,0 +1,13 @@
+package com.ts33.timejournal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*Created by TS33 on 7/7/2016.*/
+public class CalculationUnitTest {
+
+    @Test
+    public void calcTest() throws Exception{
+        assertEquals(4/2, 2);
+    }
+}
